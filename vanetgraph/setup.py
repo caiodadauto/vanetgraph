721993833e8a1bd9ad97ac929f61c6aef9b77ea5
Caio Vinicius Dadauto <caiodadauto@gmail.com>
2024-12-24 14:52:17 -0300

    setup

--- vanetgraph/setup.py
+++ vanetgraph/setup.py
@@ -1,5 +1,6 @@
 import numpy as np
 from setuptools import Extension, setup, find_packages
+
 # from Cython.Build import cythonize, build_ext
 from Cython.Distutils import build_ext
 
@@ -9,21 +10,19 @@ ext_modules = [
         "vanetgraph.utils.constructor",
         ["vanetgraph/utils/constructor.pyx"],
         include_dirs=["vanetgraph/constructor/", np.get_include(), "."],
-        extra_compile_args=['-fopenmp'],
-        extra_link_args=['-fopenmp'],
-        language="c++"
+        extra_compile_args=["-fopenmp"],
+        extra_link_args=["-fopenmp"],
+        language="c++",
     )
 ]
 
 print(find_packages())
 setup(
     name="vanetgraph",
-    author='Caio Dadauto',
-    author_email='caiodadauto@gmail.com',
+    author="Caio Dadauto",
+    author_email="caiodadauto@gmail.com",
     packages=find_packages(),
-    ext_modules = ext_modules,
-    cmdclass = {'build_ext': build_ext},
-    zip_safe=False
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
 )
-
-
